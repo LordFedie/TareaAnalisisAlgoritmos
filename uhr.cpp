a48b301a615d39de224dce43ab8abadca2cb8580
Nodercif <gamunoz2022@udec.cl>
2025-04-19 15:28:42 -0400

    Modificacion en uhr

--- uhr.cpp
+++ uhr.cpp
@@ -20,101 +20,104 @@
 #include "FuerzaBruta.h"
 #include "utils.cpp"
 #include "quartiles_nth.cpp"
-#include "Dividir&Conquistar.cpp"
+#include "DividirPorConquistar.h"
 
 // Include to be tested files here
 
 int main(int argc, char *argv[])
 {
-    // Validate and sanitize input
-    std::int64_t runs, lower, upper, step;
-    validate_input(argc, argv, runs, lower, upper, step);
+  // Validate and sanitize input
+  std::int64_t runs, lower, upper, step;
+  validate_input(argc, argv, runs, lower, upper, step);
 
-    // Set up clock variables
-    std::int64_t n, i, executed_runs;
-    std::int64_t total_runs_additive = runs * (((upper - lower) / step) + 1);
-    std::int64_t total_runs_multiplicative = runs * (floor(log(upper / double(lower)) / log(step)) + 1);
-    std::vector<double> times(runs);
-    std::vector<double> q;
-    double mean_time, time_stdev, dev;
-    auto begin_time = std::chrono::high_resolution_clock::now();
-    auto end_time = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double, std::nano> elapsed_time = end_time - begin_time;
+  // Set up clock variables
+  std::int64_t n, i, executed_runs;
+  std::int64_t total_runs_additive = runs * (((upper - lower) / step) + 1);
+  std::int64_t total_runs_multiplicative = runs * (floor(log(upper / double(lower)) / log(step)) + 1);
+  std::vector<double> times(runs);
+  std::vector<double> q;
+  double mean_time, time_stdev, dev;
+  auto begin_time = std::chrono::high_resolution_clock::now();
+  auto end_time = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double, std::nano> elapsed_time = end_time - begin_time;
 
-    // Set up random number generation
-    std::random_device rd;
-    std::mt19937_64 rng(rd());
-    std::uniform_int_distribution<std::int64_t> u_distr; // change depending on app
+  // Set up random number generation
+  std::random_device rd;
+  std::mt19937_64 rng(rd());
+  std::uniform_int_distribution<std::int64_t> u_distr; // change depending on app
 
-    // File to write time data
-    std::ofstream time_data;
-    time_data.open(argv[1]);
-    time_data << "n,t_mean,t_stdev,t_Q0,t_Q1,t_Q2,t_Q3,t_Q4" << std::endl;
+  // File to write time data
+  std::ofstream time_data;
+  time_data.open(argv[1]);
+  time_data << "n,t_mean,t_stdev,t_Q0,t_Q1,t_Q2,t_Q3,t_Q4" << std::endl;
 
-    // Begin testing
-    std::cerr << "\033[0;36mRunning tests...\033[0m" << std::endl << std::endl;
-    executed_runs = 0;
-    
-    for (n = lower; n <= upper; n += step) {
-        mean_time = 0;
-        time_stdev = 0;
+  // Begin testing
+  std::cerr << "\033[0;36mRunning tests...\033[0m" << std::endl
+            << std::endl;
+  executed_runs = 0;
 
-        // Test configuration goes here
+  for (n = lower; n <= upper; n += step)
+  {
+    mean_time = 0;
+    time_stdev = 0;
 
-        pair<double,double> arr[n];
+    // Test configuration goes here
 
-        /*
-        double resultado;
-        vector <pair<double,double>> par;
-        */
+    pair<double, double> arr[n];
 
-        // Run to compute elapsed time
-        for (i = 0; i < runs; i++) {
-            // Remember to change total depending on step type
-            display_progress(++executed_runs, total_runs_additive);
+    /*
+    double resultado;
+    vector <pair<double,double>> par;
+    */
 
-            begin_time = std::chrono::high_resolution_clock::now();
+    // Run to compute elapsed time
+    for (i = 0; i < runs; i++)
+    {
+      // Remember to change total depending on step type
+      display_progress(++executed_runs, total_runs_additive);
 
-            
-            n_random(arr,n);
-            brute_force(arr,n);
-            
+      begin_time = std::chrono::high_resolution_clock::now();
 
-            /*
-            generateRandomPoints(par,100,rng);
-            resultado = dividirPorConquistar(par,0,par.size());
-            */
+      n_random(arr, n);
+      brute_force(arr, n);
 
-            end_time = std::chrono::high_resolution_clock::now();
+      /*
+      n_random(arr, n);
+      divide_and_conquer(arr, n);
+      */
 
-            elapsed_time = end_time - begin_time;
-            times[i] = elapsed_time.count();
+      end_time = std::chrono::high_resolution_clock::now();
 
-            mean_time += times[i];
-        }
+      elapsed_time = end_time - begin_time;
+      times[i] = elapsed_time.count();
 
-        // Compute statistics
-        mean_time /= runs;
-
-        for (i = 0; i < runs; i++) {
-            dev = times[i] - mean_time;
-            time_stdev += dev * dev;
-        }
-
-        time_stdev /= runs - 1; // Subtract 1 to get unbiased estimator
-        time_stdev = std::sqrt(time_stdev);
-
-        quartiles(times, q);
-
-        time_data << n << "," << mean_time << "," << time_stdev << ",";
-        time_data << q[0] << "," << q[1] << "," << q[2] << "," << q[3] << "," << q[4] << std::endl;
+      mean_time += times[i];
     }
 
-    // This is to keep loading bar after testing
-    std::cerr << std::endl << std::endl;
-    std::cerr << "\033[1;32mDone!\033[0m" << std::endl;
+    // Compute statistics
+    mean_time /= runs;
 
-    time_data.close();
+    for (i = 0; i < runs; i++)
+    {
+      dev = times[i] - mean_time;
+      time_stdev += dev * dev;
+    }
 
-    return 0;
+    time_stdev /= runs - 1; // Subtract 1 to get unbiased estimator
+    time_stdev = std::sqrt(time_stdev);
+
+    quartiles(times, q);
+
+    time_data << n << "," << mean_time << "," << time_stdev << ",";
+    time_data << q[0] << "," << q[1] << "," << q[2] << "," << q[3] << "," << q[4] << std::endl;
+  }
+
+  // This is to keep loading bar after testing
+  std::cerr << std::endl
+            << std::endl;
+  std::cerr << "\033[1;32mDone!\033[0m" << std::endl;
+
+  time_data.close();
+
+  return 0;
 }
